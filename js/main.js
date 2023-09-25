c33d15368920d5f407663b417d790d1a9ae64249
alif-js <alif.hubspot@gmail.com>
2023-09-25 21:05:29 +0600

    code not perfect. still, final commit for now

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ document.getElementById('btn-pin').addEventListener('click', function(){
     const generatedPinField = document.getElementById('generated-pin-field');
     generatedPinField.value = generatedPin;
 })
+
+document.getElementById('pin-input-body').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    
+    const typedPinField = document.getElementById('typed-pin-field');
+    typedPinField.value = typedPinField.value + number;
+})
+
+const pinSuccessMessage = document.getElementById('pin-success');
+const pinFailureMessage = document.getElementById('pin-failure');
+
+document.getElementById('btn-submit').addEventListener('click', function(){
+    if(document.getElementById('generated-pin-field').value === document.getElementById('typed-pin-field').value){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else{
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
+})
